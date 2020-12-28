292418b0613e05bb55df36985dcfcd5c85906d93
Alina <nyrete@gmail.com>
2020-12-29 01:27:31 +0500

    Редакция папки .github

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -2,7 +2,7 @@ package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
