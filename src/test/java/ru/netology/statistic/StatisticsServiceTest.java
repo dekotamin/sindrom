b88ec64424aabe7a0150f1494889bc5186dcb317
Alina <nyrete@gmail.com>
2021-01-18 20:12:37 +0500

    Покрытие 100%

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -14,4 +14,12 @@ class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
+    @Test
+    void findMaxthrowCurrent_Max_Index() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
 }
